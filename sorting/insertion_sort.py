f93204b2ed2e54439a63bcc73922f76072711ae0
supersxk <supersxk@gmail.com>
2019-06-22 18:56:58 +0800

    optimize insertion sort according to pseudocode from book - algorithms in a nutshell 2ed

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -1,7 +1,7 @@
 class Sort:
 
     @staticmethod
-    def insertionSort(arr):
+    def insertion_sort(arr):
         # No need to sort
         if arr is None:
             return arr
@@ -14,21 +14,17 @@ class Sort:
         # j = range in order
         for i in range(1, n):
             value = arr[i]
-            j = i
+            j = i - 1
 
             # find the right position for the new element from out-of-order range
-            while j >= 0:
+            # and move elements greater than value to the right
+            while j >= 0 and arr[j] > value:
+                arr[j + 1] = arr[j]
                 j -= 1
-                if j < 0:   # FIXME how to write more gracefully?
-                    break
-                if arr[j] > value:
-                    arr[j + 1] = arr[j]  # move data
-                else:
-                    break
             arr[j + 1] = value  # place the element from out-of-order range into the right position
 
         return arr
 
 
 if __name__ == '__main__':
-    print(Sort.insertionSort([4, 5, 6, 1, 3, 2]))
+    print(Sort.insertion_sort([4, 5, 6, 1, 3, 2]))
