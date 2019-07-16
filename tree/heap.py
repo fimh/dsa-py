b5f4968f54574d3cff55a9520a0a445b2d32d029
supersxk <supersxk@gmail.com>
2019-07-16 15:51:38 +0800

    add sample code for heap (max-heap)

---
+++ tree/heap.py
@@ -0,0 +1,109 @@
+class Heap:
+    """
+    This is a max-heap with root node starting from 1.
+    """
+
+    def __init__(self, capacity: int):
+        self.arr = [0] * (capacity + 1)
+        self.max_size = capacity
+        self.count = 0
+
+    def insert(self, data: int):
+        if self.count >= self.max_size:  # heap is full
+            return
+
+        # add the new element to the tail
+        self.count += 1
+        self.arr[self.count] = data
+
+        # heapify from bottom to top
+        self.heapify_upwards_1_based(self.arr, self.count)
+
+    def remove_top(self):
+        if self.count == 0:  # heap is empty
+            return
+
+        # copy the tail element to the top, then remove the tail element
+        self.arr[1] = self.arr[self.count]
+        self.count -= 1
+
+        # heapify from top to bottom
+        self.heapify_downwards_1_based(self.arr, self.count, 1)
+
+    def get_top(self):
+        if self.count == 0:
+            return None
+
+        return self.arr[1]
+
+    @staticmethod
+    def heapify_upwards_1_based(arr, i):
+        """
+        Heapify the given array at index = i from bottom to top.
+        :param arr: List[int], the heap array
+        :param i: int, the starting index to heapify
+        :return: void, the operation is in place
+        """
+
+        while i // 2 > 0 and arr[i] > arr[i // 2]:
+            arr[i], arr[i // 2] = arr[i // 2], arr[i]
+            i = i // 2
+
+    @staticmethod
+    def heapify_downwards_0_based(arr, n, i):
+        """
+        Heapify the given array at index = i from top to bottom.
+        The root node of heap will start from 0.
+        :param arr: List[int], the heap array
+        :param n: int, the heap size
+        :param i: int, the starting index to heapify
+        :return: void, the operation is in place
+        """
+
+        while True:
+            max_pos = i
+            if i * 2 + 1 <= n - 1 and arr[i] < arr[i * 2 + 1]:
+                max_pos = i * 2 + 1
+            if i * 2 + 2 <= n - 1 and arr[max_pos] < arr[i * 2 + 2]:
+                max_pos = i * 2 + 2
+            if max_pos == i:
+                break
+
+            arr[i], arr[max_pos] = arr[max_pos], arr[i]
+            i = max_pos
+
+    @staticmethod
+    def heapify_downwards_1_based(arr, n, i):
+        """
+        Heapify the given array at index = i from top to bottom.
+        The root node of heap will start from 1.
+        :param arr: List[int], the heap array
+        :param n: int, the heap size
+        :param i: int, the starting index to heapify
+        :return: void, the operation is in place
+        """
+
+        while True:
+            max_pos = i
+            if i * 2 <= n and arr[i] < arr[i * 2]:
+                max_pos = i * 2
+            if i * 2 + 1 <= n and arr[max_pos] < arr[i * 2 + 1]:
+                max_pos = i * 2 + 1
+            if max_pos == i:
+                break
+
+            arr[i], arr[max_pos] = arr[max_pos], arr[i]
+            i = max_pos
+
+
+if __name__ == '__main__':
+    heap = Heap(12)
+
+    arr = [32, 64, 20, 27, 56, 40]
+    for i in arr:
+        heap.insert(i)
+    print(heap.arr, heap.count)
+
+    for i in range(2):
+        heap.remove_top()
+        print(heap.arr, heap.count)
